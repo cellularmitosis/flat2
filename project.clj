c7b5d99e4380565803629c285060d12319df79f0
Jason Pepas <jasonpepas@gmail.com>
2019-07-17 14:50:08 -0500

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,3 +1,3 @@
-(defproject minimal-clojure-project/lein-template "0.1.0-SNAPSHOT"
+(defproject main/lein-template "0.1.0"
   :description "Leiningen template for minimal Clojure project."
   :eval-in-leiningen true)
