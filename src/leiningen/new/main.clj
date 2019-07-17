d4dcf93796a144d97016bccaa1dc151689416d08
Jason Pepas <jasonpepas@gmail.com>
2019-07-17 14:52:09 -0500

    Update and rename minimal_clojure_project.clj to main.clj

--- src/leiningen/new/main.clj
+++ src/leiningen/new/main.clj
@@ -1,17 +1,17 @@
-(ns leiningen.new.minimal-clojure-project
+(ns leiningen.new.main
   (:require [leiningen.new.templates :refer [renderer name-to-path ->files]]
             [leiningen.core.main :as main]))
 
-(def render (renderer "minimal-clojure-project"))
+(def render (renderer "main"))
 
-(defn minimal-clojure-project
+(defn main
   "Leiningen template for minimal Clojure project."
   [name]
   (let [data {:name name
               :sanitized (name-to-path name)}]
-    (main/info "Generating fresh 'lein new' minimal-clojure-project project.")
+    (main/info "Generating fresh 'lein new' main project.")
     (->files data
-             ["README.markdown" (render "README.markdown" data)]
+             ["README.md" (render "README.md" data)]
              ["project.clj" (render "project.clj" data)]
              ["main.clj" (render "main.clj" data)]
              [".gitignore" (render "gitignore" data)])))
